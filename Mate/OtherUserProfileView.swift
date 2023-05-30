3431da37b47e0720cf3bf1b321edd21b2300ee18
moo20222 <samuel@ou.edu>
2023-05-30 12:29:40 -0500

    UI for other users

--- Mate/OtherUserProfileView.swift
+++ Mate/OtherUserProfileView.swift
@@ -6,32 +6,72 @@
 //
 import Firebase
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct OtherUserProfileView: View {
     @Environment(\.presentationMode) var presentationMode
     var username: String
+    var profileImage: String
     
     var body: some View {
-        Text("Profile of \(username)")
-            .navigationBarTitle(Text(username), displayMode: .inline)
-            .navigationBarItems(leading: backButton) // Apply custom appearance to the back button
-            .accentColor(.black) // Set the accent color to blue
+        VStack {
+            HStack (spacing: 16) {
+                VStack{
+                    Text("Followers")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                    Text("10k")
+                        .font(.headline)
+                }.padding()
+                VStack {
+                    //                Text("Profile of \(username)")
+                    //                    .navigationBarTitle(Text(username.lowercased()), displayMode: .inline).textCase(.lowercase)
+                    //                    .navigationBarItems(leading: backButton)
+                    //                    .accentColor(.black)
+                    WebImage(url: URL(string: profileImage))
+                        .resizable()
+                        .frame(width: 82, height: 84)
+                        .clipShape(Circle())
+                        .foregroundColor(Color.black)
+                        .clipped()
+                        .overlay(Circle().stroke(Color.black, lineWidth: 2))
+                        .padding()
+                        .foregroundColor(Color.black)
+                }
+                VStack {
+                    Text("Following")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                    Text("5k")
+                        .font(.headline)
+                }
+                .padding()
+            }
+            Spacer()
+        }
+        .navigationBarTitle(Text(username.lowercased()), displayMode: .inline)
+        .textCase(.lowercase)
+        .navigationBarItems(leading: backButton)
+        .accentColor(.black)
+        .navigationBarBackButtonHidden(true)
     }
     
     private var backButton: some View {
-            Button(action: {
-                presentationMode.wrappedValue.dismiss()
-            }) {
-                Image(systemName: "chevron.left")
-                    .imageScale(.large)
-            }
+        Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        }) {
+            Image(systemName: "chevron.left")
+                .imageScale(.large)
         }
+    }
 }
 
 
 struct OtherUserProfileView_Previews: PreviewProvider {
     static var previews: some View {
         @State var username: String = ""
-        OtherUserProfileView(username: username)
+        @State var profileImage: String = ""
+
+        OtherUserProfileView(username: username, profileImage: profileImage)
     }
 }
